1f29dd7aced9652e91be418f513a2ee7a695feaa
Aymeric Artero <arteroaymeric.dev@gmail.com>
2021-09-10 16:07:01 +0200

    Add acception formulaire

--- front/assets/js/inscription.js
+++ front/assets/js/inscription.js
@@ -8,17 +8,24 @@ window.addEventListener('DOMContentLoaded', event => {
 let transformPlaceholder = () => {
     let allDiv = document.getElementsByClassName('form-group');
 
-
+    /** Traduction nom" **/
     allDiv[0].children.item(1).placeholder = "Votre nom";
+
+    /** Traduction email **/
     allDiv[1].children.item(1).placeholder = "Votre email";
+
+    /** Traduction message **/
     allDiv[2].children.item(1).placeholder = "Votre message";
+
 }
 
 let addRgpdDiv = () => {
     const elem = document.createElement('div');
 
     elem.className = "text-muted politique"
-    elem.innerHTML = "En cliquant sur Envoyer, vous acceptez notre <a href='https://tag.aymeric.tech/conditions/use'>politique d'utilisation</a> ainsi que notre <a href='https://tag.aymeric.tech/confidentialite'>politique de confidentialité</a>";
+    elem.innerHTML =
+        "<p>Une fois la demande envoyée un compte sera créé et un membre de l'équipe vous contactera.</p>" +
+        "<p>En cliquant sur Envoyer, vous acceptez notre <a href='https://tag.aymeric.tech/conditions/use'>politique d'utilisation</a> ainsi que notre <a href='https://tag.aymeric.tech/confidentialite'>politique de confidentialité</a>.</p>";
 
     const target = document.querySelector('.btn');
     target.parentNode.insertBefore(elem, target);
