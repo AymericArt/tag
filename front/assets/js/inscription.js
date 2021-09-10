ecbe3d24f90ee193893d5d9fcfce979fb293ebfb
Aymeric Artero <arteroaymeric.dev@gmail.com>
2021-09-10 12:17:05 +0200

    Add form inscription

--- front/assets/js/inscription.js
+++ front/assets/js/inscription.js
@@ -1,34 +1,9 @@
-import axios from "axios"
-
 window.addEventListener('DOMContentLoaded', event => {
-    let name    = document.getElementById("name");
-    let email   = document.getElementById("email");
-    let phone   = document.getElementById("phone");
-    let message = document.getElementById("message");
+    const elem = document.createElement('div');
 
-    document.getElementById("contactForm").addEventListener("submit", (e) => {
-        e.preventDefault();
+    elem.className = "text-muted politique"
+    elem.innerHTML = "En cliquant sur Envoyer, vous acceptez notre <a href='https://tag.aymeric.tech/conditions/use'>politique d'utilisation</a> ainsi que notre <a href='https://tag.aymeric.tech/confidentialite'>politique de confidentialité</a>";
 
-        axios.defaults.headers.post['Content-Type'] ='application/json;charset=utf-8';
-        axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
-
-        let config = {
-            headers: {
-                "Authorization": "Bearer zDba6iIX_jzlWrixgECqWj4M1IlN9Dt73HmowxMXfH9KVWGWP1XFyoByzT1scX1R",
-            }
-        }
-
-        axios.post("https://support.aymeric.tech/api/v1/users", {
-            firstname: name.value,
-            email: email.value,
-            phone: phone.value,
-            groups: "Users",
-            invite: true,
-            roles: "Mairie"
-        })
-            .then(data => {
-                console.log(data)
-            })
-
-    })
+    const target = document.querySelector('.btn');
+    target.parentNode.insertBefore(elem, target);
 });
